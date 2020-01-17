2cbea02097f6bdb55729bb667ad7bf62bfa531b1
Mladen Stojkovic <mladenstojkovic@pop-os.localdomain>
2020-01-17 06:01:28 +0100

    feat(mini-xstate): a small example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {Machine,assign, interpret} from 'xstate';
+import {useMachine} from '@xstate/react'
+import { update } from 'xstate/lib/actionTypes';
+
+function fakePayment(){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve('gotcha'),3000)
+  })
+}
+
+const stateMachine = Machine({
+  initial: 'idle',
+  context:{
+    msg:''
+  },
+  states:{
+    idle:{
+      on:{
+        //Check if the condition is true, if it is go to loading, else go to next key on this array (error)
+        SUBMIT: [{
+          target:'loading',
+          cond: (ctx,event)=>event.data.name !=='' && event.data.card!==''
+        },{
+          target:'error'
+        }]
+      }
+    },
+    loading:{
+      invoke:{
+        id:'doPayment',
+        src:(ctx,event) => fakePayment(),
+        onDone:{
+          target: 'success',
+          actions: assign({msg:(ctx,event)=>event.data}),
+        },
+        onError:{
+          target: 'error',
+          actions: assign({msg:(ctx,event)=>event.data}),
+        }
+      }
+    },
+    error:{
+      on:{
+        SUBMIT:{
+          target:'loading',
+          cond: (ctx,event)=>event.data.name !=='' && event.data.card!==''
+        }
+      }
+    },
+    success:{
+      type:'final'
+    }
+  }
+});
+
 
 function App() {
+  const [machine,send] = useMachine(stateMachine);
+   console.log([machine,send]);
+  const [form,updateForm] = useState({
+    name:'',
+    card:''
+  })
+  console.log(form);
+  // console.log(machine.value);
+  //const service = interpret(machine).start()
+  console.log(service);
+  
+  // machine.onTransition(state=>console.log(state.value)
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="form-container">
+      <div className="form-header">
+        <h2>State Machine Payment Form</h2>
+      </div>
+
+    {machine.matches('error')?(
+      <div className="alert error">
+        {machine.context.msg ? machine.context.msg : 'No errors here lmao'}
+      </div>
+    ):null}
+      <div className="form-body">
+        <form
+          onSubmit={
+            e=>{e.preventDefault();
+              send({type:'SUBMIT',data:{...form}})
+            }
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <div className="form-group">
+            <label htmlFor="NameOnCard">Name on card</label>
+            <input
+              id="NameOnCard"
+              className="form-control"
+              type="text"
+              maxLength="255"
+              value={form.name}
+              onChange={e=>updateForm({...form,name:e.target.value})}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="CreditCardNumber">Card number</label>
+            <input
+              id="CreditCardNumber"
+              className="null card-image form-control"
+              type="text"
+              value={form.card}
+              onChange={e=>updateForm({...form,card:e.target.value})}
+            />
+          </div>
+          <button
+            id="PayButton"
+            className="btn btn-block btn-success submit-button"
+            type="submit"
+          >
+            <span className="submit-button-lock" />
+            <span className="align-middle">Pay Now</span>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
